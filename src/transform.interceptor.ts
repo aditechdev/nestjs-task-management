8a2cc074e3e0b5f5978fb58d06e1c84d424a85f4
Aditya Anand <contactaditechdev@gmail.com>
2026-05-17 18:27:02 +0530

    :sparkles: Transform interceptor implemented

---
+++ src/transform.interceptor.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  NestInterceptor,
+  ExecutionContext,
+  Injectable,
+  CallHandler,
+} from '@nestjs/common';
+
+import { instanceToPlain } from 'class-transformer';
+
+import { Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+
+    next: CallHandler,
+  ): Observable<any> {
+    return next.handle().pipe(map((data) => instanceToPlain(data)));
+  }
+}
